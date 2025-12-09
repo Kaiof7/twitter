ae228d7d27f652e7639acab2d4628e6ac792e9aa
Kaio Felipe <kaiofelipee12@gmail.com>
2025-12-09 09:42:11 -0300

    agora vai?

--- backend/twitter_clone/urls.py
+++ backend/twitter_clone/urls.py
@@ -1,4 +1,3 @@
-# twitter_clone/urls.py
 from twitter_clone import views
 from django.contrib import admin
 from django.urls import path, include
@@ -7,12 +6,14 @@ from django.http import HttpResponse
 
 urlpatterns = [
     path("", lambda request: HttpResponse("API do Twitter Clone está funcionando.")),
-    
+
     path('admin/', admin.site.urls),
     path('api/', include('tweets.urls')),
     path('api/users/', include('users.urls')),
-    path("api/token/"),
-    path("api/token/refresh/"),
+
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+
     path("update_server/", views.update, name="update"),
     path("hello/", views.hello_world, name="hello_world"),
 ]
